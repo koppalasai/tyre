1fc3b17533d70a141f1c3b640612b70d7e810818
koppalasai <koppalasaisantosh@gmail.com>
2025-11-12 19:17:14 +0530

    Updated code after modifications

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: 'bundle-report.html', // report file name
+      open: true,                     // opens report automatically after build
+      gzipSize: true,                 // include gzip size
+      brotliSize: true,               // include brotli size
+    }),
+  ],
   server: {
     watch: {
       usePolling: true,
